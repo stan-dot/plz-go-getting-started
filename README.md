649436322472099aef5e0dc49aed127974ea6b1a
root <root@pc0143.cs.diamond.ac.uk>
2024-02-23 11:30:32 +0000

    all ready

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ go_toolchain(
 )
 `
 not `go_toolchain`
+
+## a convoluted way to add third party deps
+https://please.build/codelabs/go_intro/index.html#8
+
+
+and then the auto-geneerated list of indirect modules included (unnecessarily) a package for the original lib itself.
+that caused `plz test` to break
+https://please.build/codelabs/go_intro/index.html#8
+
+
--- src/greetings/greetings_test.go
+++ src/greetings/greetings_test.go
@@ -3,6 +3,7 @@ package greetings_test
 import (
 	// importing the "production" package
 	"github.com/example/module/src/greetings"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -10,4 +11,6 @@ func TestGreeting(t *testing.T) {
 	if greetings.Greeting() == "" {
 		panic("Greeting failed to produce a result")
 	}
+
+	assert.NotEqual(t, "", greetings.Greeting())
 }
